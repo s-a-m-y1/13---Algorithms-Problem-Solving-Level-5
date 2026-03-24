615d350823bdf37431d52aa9dc792789d779672e
s-a-m-y1 <sam858y@gmail.com>
2026-03-24 23:23:09 +0200

    Update Stack Data Structures

--- Ds/clsMyStack.h
+++ Ds/clsMyStack.h
@@ -1,78 +1,26 @@
 #pragma once
 #include<iostream>
 #include"clsDblLinkedList.h"
+#include"clsMyQueue.h"
 using namespace std;
 template<class T>
-class clsMyStack
+class clsMyStack:public clsMyQueue <T>
 {
-private:
-	clsDblLinkedList <T >  Stack;
 public:
-
 	void push(T Val)
 	{
-		Stack.InsertAtBeginning(Val);
-	}
-	void pop()
-	{
-		Stack.DeleteLastNode();
-	}
-
-	void print()
-	{
-		Stack.PrintList();
-	}
-
-	bool Isempty()
-	{
-		return(Stack.ISEmpty());
-	}
-	int Size()
-	{
-
-		return(Stack.Size());
+		clsMyQueue<T>::Queue.InsertAtBeginning(Val);
 	}
 
 	T Top()
 	{
-		return(Stack.GetItem(0));
+
+		clsMyQueue<T>::front();
 	}
+
 	T Bottom()
 	{
-
-		return(Stack.GetItem(Size() - 1));
-	}
-
-	T Getitem(int pos)
-	{
-		return(Stack.GetItem(pos));
-	}
-	void Revers()
-	{
-
-		Stack.Revers();
-	}
-
-	void UpdateItem(int pos , T Replase )
-	{
-		Stack.UpdateItem(pos, Replase);
-	}
-
-	bool Insertafter(int pos , T Val)
-	{
-		return(Stack.InsertAfter(pos, Val));
-	}
-	bool InsertatFront(T val)
-	{
-		return(Stack.InsertAfter(0, val));
-	}
-	bool InsertatBack(T val)
-	{
-		return(Stack.InsertAfter(Size()-1, val));
-	}
-	void Clear()
-	{
-		Stack.Clear();
+		clsMyQueue<T>::back();
 	}
 };
 
